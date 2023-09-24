61abc88f86eb01c1e00767de11afa1d9b9ffd5db
Ev-jan <evgen.mazyar@gmail.com>
2023-09-24 21:18:48 +0400

    update vite config

--- src/pages/Results/components/DocListSection.tsx
+++ src/pages/Results/components/DocListSection.tsx
@@ -152,14 +152,3 @@ margin: 0 auto;
 max-width: 305px;
 grid-column: 1 / 3;
 `
-
-// sf_student1	4i2385j
-// sf_student10	KHKfTXb
-// sf_student3	6z9ZFRs
-// sf_student2	lV8xjCH
-// sf_student4	Br1+tbG
-// sf_student5	LuwAwJf
-// sf_student6	eczpWCB
-// sf_student7	P6VcKNf
-// sf_student8	5QB0KM/
-// sf_student9	DTdEwAn*/
